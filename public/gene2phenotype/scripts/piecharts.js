5206bd81b9a1211d34b706c6914d484bfb86e768
Anja Thormann <anja@ebi.ac.uk>
2016-08-10 14:09:06 +0100

    formatting

--- public/gene2phenotype/scripts/piecharts.js
+++ public/gene2phenotype/scripts/piecharts.js
@@ -1,26 +1,27 @@
 $( document ).ready(function() {
   nv.addGraph(function() {
-    d3.selectAll('.chart').call(function(div){
-      var chart = nv.models.pieChart()
+    if ($('.chart').length) {
+      d3.selectAll('.chart').call(function(div){
+        var chart = nv.models.pieChart()
         .x(function(d)     { return d.label })
         .y(function(d)     { return d.value })
         .showLabels(false)
         .width(600)
         .height(400);
 
-      chart.color(function(d){ return d.data.color });
+        chart.color(function(d){ return d.data.color });
 
-      var data = JSON.parse(div.attr('data'));
-      var data = data.sort(function(a, b) { return a.label.localeCompare(b.label) });
+        var data = JSON.parse(div.attr('data'));
+        var data = data.sort(function(a, b) { return a.label.localeCompare(b.label) });
 
-  //var data = [{"value":1,"label":"not assigned"},{"value":2,"label":"Nonsense"},{"value":1,"label":"Frameshift"},{"value":6,"label":"Missense/In-frame"}];
-  //    var data = [{"label": "One","value" : 50,} ,{"label": "Two","value" : 50,} ,];
+        //var data = [{"value":1,"label":"not assigned"},{"value":2,"label":"Nonsense"},{"value":1,"label":"Frameshift"},{"value":6,"label":"Missense/In-frame"}];
+        //var data = [{"label": "One","value" : 50,} ,{"label": "Two","value" : 50,} ,];
 
-      div.select('svg').datum(data)
+        div.select('svg').datum(data)
         .transition().duration(1200)
         .call(chart);
-      return chart;
-    });
+        return chart;
+      });
+    }
   });
 });
-
