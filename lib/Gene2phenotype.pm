761053a9c8052bfe503b2bdc737107c02bb26b82
Anja Thormann <anja@ebi.ac.uk>
2017-10-25 14:55:18 +0100

    add g2p vep documentation

--- lib/Gene2phenotype.pm
+++ lib/Gene2phenotype.pm
@@ -391,6 +391,8 @@ sub startup {
     $c->cgi->run( script => "$home/cgi-bin/$script_name");
   });
   $r->get('/gene2phenotype/downloads')->to(template => 'downloads');
+  $r->get('/gene2phenotype/g2p_vep_plugin')->to(template => 'g2p_vep_plugin');
+
   $r->get('/gene2phenotype/downloads/#file_name' => sub {
     my $c = shift;
     my $file_name = $c->stash('file_name');
