17637139b59083afcf754a8443295d30f6531344
Anja Thormann <anja@ebi.ac.uk>
2018-07-30 17:37:46 +0100

    link to about page

--- lib/Gene2phenotype.pm
+++ lib/Gene2phenotype.pm
@@ -410,6 +410,7 @@ sub startup {
     $c->cgi->run( script => "$home/cgi-bin/$script_name");
   });
   $r->get('/gene2phenotype/downloads')->to(template => 'downloads');
+  $r->get('/gene2phenotype/about')->to(template => 'about');
   $r->get('/gene2phenotype/g2p_vep_plugin')->to(template => 'g2p_vep_plugin');
 
   $r->get('/gene2phenotype/downloads/#file_name' => sub {
