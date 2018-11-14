1091a30f8df378fbe15189e91f3a5282b5bc9232
Anja Thormann <anja@ebi.ac.uk>
2018-11-14 12:54:49 +0000

    decide what to show based on logged_in value

--- lib/Gene2phenotype/Model/Home.pm
+++ lib/Gene2phenotype/Model/Home.pm
@@ -31,22 +31,20 @@ sub fetch_updates {
 
   my @results = ();
 
-  my @panels = map { $_->name } @{$panel_adaptor->fetch_all_visible_Panels}; # set in panel table: is_visible
-  foreach my $panel (@{$panel_adaptor->fetch_all_visible_Panels}) {
+#  my @panels = map { $_->name } @{$panel_adaptor->fetch_all_visible_Panels}; # set in panel table: is_visible
+#  foreach my $panel (@{$panel_adaptor->fetch_all_visible_Panels}) {
     # if in authorised panels (curator can edit content) show all entries 
     # if panel is not in authorised_panels only show visible entries
-    my $only_visible_entries = (!grep{$_ eq $panel->name} @$authorised_panels) ? 1 : 0;
+#    my $only_visible_entries = (!grep{$_ eq $panel->name} @$authorised_panels) ? 1 : 0;
     # and logged in
-    my $updates = $GFDL_adaptor->fetch_latest_updates($panel->name, 10, $only_visible_entries);
-    push @results, { panel => $panel->name, updates => $self->format_updates($updates)};
-  }  
-
+#    my $updates = $GFDL_adaptor->fetch_latest_updates($panel->name, 10, $only_visible_entries);
+#    push @results, { panel => $panel->name, updates => $self->format_updates($updates)};
+#  }  
+  my $only_visible_entries = ($logged_in) ? 0 : 1;
   foreach my $panel_name (@$authorised_panels) {
     next if ($panel_name eq 'ALL');
-    if (!grep{$_ eq $panel_name} @panels) {
-      my $updates = $GFDL_adaptor->fetch_latest_updates($panel_name, 10);
-      push @results, { panel => $panel_name, updates => $self->format_updates($updates)};
-    }
+    my $updates = $GFDL_adaptor->fetch_latest_updates($panel_name, 10, $only_visible_entries);
+    push @results, { panel => $panel_name, updates => $self->format_updates($updates)};
   }
   return \@results;
 }
