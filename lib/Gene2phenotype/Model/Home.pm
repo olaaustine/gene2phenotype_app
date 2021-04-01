5e570b60c438c749abc9e23346276c9ecf276b0a
at7 <anja@ebi.ac.uk>
2021-04-01 15:39:38 +0100

    cleanup

--- lib/Gene2phenotype/Model/Home.pm
+++ lib/Gene2phenotype/Model/Home.pm
@@ -18,23 +18,14 @@ limitations under the License.
 package Gene2phenotype::Model::Home;
 use Mojo::Base 'MojoX::Model';
 use JSON;
+
 sub fetch_statistics {
   my $self = shift;
   my $logged_in = shift;
   my $authorised_panels = shift;
   my $registry = $self->app->defaults('registry');
-  my $GFD_adaptor = $registry->get_adaptor('human', 'gene2phenotype', 'genomicfeaturedisease');
-  my $attribute_adaptor = $registry->get_adaptor('human', 'gene2phenotype', 'attribute');
-  my $panel_adaptor = $registry->get_adaptor('human', 'gene2phenotype', 'panel');
-
-  my @panel_names = map {$_->name} @{$panel_adaptor->fetch_all_visible_Panels};
-  my @panels = ();
-  foreach my $panel_name (@$authorised_panels, @panel_names) {
-    next if ($panel_name eq 'ALL');
-    my $panel_id = $attribute_adaptor->attrib_id_for_value($panel_name);
-    push @panels, $panel_id;
-  }
-  my $stats = $GFD_adaptor->get_statistics(\@panels); 
+  my $GFD_panel_adaptor = $registry->get_adaptor('human', 'gene2phenotype', 'GenomicFeatureDiseasePanel');
+  my $stats = $GFD_panel_adaptor->get_statistics(); 
   return JSON::to_json($stats);
 }
 
@@ -43,25 +34,13 @@ sub fetch_updates {
   my $logged_in = shift;
   my $authorised_panels = shift;
   my $registry = $self->app->defaults('registry');
-  my $GFDL_adaptor = $registry->get_adaptor('human', 'gene2phenotype', 'genomicfeaturediseaselog');
-
+  my $GFD_panel_log_adaptor = $registry->get_adaptor('human', 'gene2phenotype', 'GenomicFeatureDiseasePanelLog');
   my $panel_adaptor = $registry->get_adaptor('human', 'gene2phenotype', 'panel');
-
   my @results = ();
-
-#  my @panels = map { $_->name } @{$panel_adaptor->fetch_all_visible_Panels}; # set in panel table: is_visible
-#  foreach my $panel (@{$panel_adaptor->fetch_all_visible_Panels}) {
-    # if in authorised panels (curator can edit content) show all entries 
-    # if panel is not in authorised_panels only show visible entries
-#    my $only_visible_entries = (!grep{$_ eq $panel->name} @$authorised_panels) ? 1 : 0;
-    # and logged in
-#    my $updates = $GFDL_adaptor->fetch_latest_updates($panel->name, 10, $only_visible_entries);
-#    push @results, { panel => $panel->name, updates => $self->format_updates($updates)};
-#  }  
   my $only_visible_entries = ($logged_in) ? 0 : 1;
   foreach my $panel_name (@$authorised_panels) {
     next if ($panel_name eq 'ALL');
-    my $updates = $GFDL_adaptor->fetch_latest_updates($panel_name, 10, $only_visible_entries);
+    my $updates = $GFD_panel_log_adaptor->fetch_latest_updates($panel_name, 10, $only_visible_entries);
     push @results, { panel => $panel_name, updates => $self->format_updates($updates)};
   }
   return \@results;
@@ -71,7 +50,7 @@ sub format_updates {
   my $self = shift;
   my $updates = shift;
   my $registry = $self->app->defaults('registry');
-  my $GFD_adaptor = $registry->get_adaptor('human', 'gene2phenotype', 'genomicfeaturedisease');
+  my $GFD_adaptor = $registry->get_adaptor('human', 'gene2phenotype', 'GenomicFeatureDisease');
 
   my @results = ();
   foreach my $update (@$updates) {
@@ -84,11 +63,11 @@ sub format_updates {
     my $mutation_consequence = $gfd->mutation_consequence || 'not specified';
     push @results, {
       date => $date,
-      gene_symbol => $update->gene_symbol,
-      disease_name => $update->disease_name,
+      gene_symbol => $gfd->get_GenomicFeature->gene_symbol,
+      disease_name => $gfd->get_Disease->name,
       genotype => $allelic_requirement,
       mechanism => $mutation_consequence,
-      GFD_ID => $update->genomic_feature_disease_id,
+      GFD_ID => $gfd->dbID,
       search_type => 'gfd' 
     };
   }
