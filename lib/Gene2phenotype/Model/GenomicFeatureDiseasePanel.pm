e9b4208ee35e115bf08c892e339e2a0d281d083f
at7 <anja@ebi.ac.uk>
2021-07-20 15:23:54 +0100

    update documentation

--- lib/Gene2phenotype/Model/GenomicFeatureDiseasePanel.pm
+++ lib/Gene2phenotype/Model/GenomicFeatureDiseasePanel.pm
@@ -1,22 +1,39 @@
 =head1 LICENSE
- 
+
 See the NOTICE file distributed with this work for additional information
 regarding copyright ownership.
- 
+
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 http://www.apache.org/licenses/LICENSE-2.0
- 
+
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
- 
+
 =cut
+
 package Gene2phenotype::Model::GenomicFeatureDiseasePanel; 
 use Mojo::Base 'MojoX::Model';
+
+=head2 add
+  Arg [1]    : Integer $gfd_id - Database id of GenomicFeatureDisease
+  Arg [2]    : String $panel - Panel to which the GenomicFeatureDisease
+               is added
+  Arg [3]    : String $confidence_value - Confidence value for new
+               GenomicFeatureDiseasePanel
+  Arg [4]    : String $email - Email of user who creates the new
+               GenomicFeatureDiseasePanel
+  Description: Add GenomicFeatureDisease to panel and thereby
+               create a new GenomicFeatureDiseasePanel
+  Returntype : None
+  Exceptions : None
+  Caller     : Gene2phenotype::Controller::GenomicFeatureDiseasePanel::add
+  Status     : Stable
+=cut
 
 sub add {
   my $self = shift;
@@ -39,6 +56,18 @@ sub add {
   return $gfd_panel;
 }
 
+=head2 update_visibility
+  Arg [1]    : String $email - Email of user who updates the visibility
+               of the GenomicFeatureDiseasePanel
+  Arg [2]    : Integer $GFD_panel_id - Database id of GenomicFeatureDiseasePanel
+  Arg [3]    : String $visibility - Value is either authorised or restricted
+  Description: Update visibility of GenomicFeatureDiseasePanel
+  Returntype : None
+  Exceptions : None
+  Caller     : Gene2phenotype::Controller::GenomicFeatureDiseasePanel::update_visibility
+  Status     : Stable
+=cut
+
 sub update_visibility {
   my $self = shift;
   my $email = shift;
@@ -54,6 +83,18 @@ sub update_visibility {
   $GFD_panel_adaptor->update($GFD_panel, $user);
 }
 
+=head2 update_confidence_category
+  Arg [1]    : String $email - Email of user who updates the confidence
+               category
+  Arg [2]    : Integer $GFD_panel_id - Database id of GenomicFeatureDiseasePanel
+  Arg [3]    : String $category_attrib_id - The new confidence category attrib
+  Description: Update the confidence category of the GenomicFeatureDiseasePanel
+  Returntype : None
+  Exceptions : None
+  Caller     : Gene2phenotype::Controller::GenomicFeatureDiseasePanel::update_confidence_category
+  Status     : Stable
+=cut
+
 sub update_confidence_category {
   my $self = shift;
   my $email = shift;
@@ -67,6 +108,17 @@ sub update_confidence_category {
   $GFD_panel->confidence_category_attrib($category_attrib_id);
   $GFD_panel_adaptor->update($GFD_panel, $user); 
 } 
+
+=head2 delete
+  Arg [1]    : String $email - Email of user who runs the delete action.
+  Arg [2]    : Integer $GFD_panel_id - database id of GenomicFeatureDiseasPanel
+               that needs to be deleted
+  Description: Delete GenomicFeatureDiseasePanel
+  Returntype : None
+  Exceptions : None
+  Caller     : Gene2phenotype::Controller::GenomicFeatureDiseasePanel::delete
+  Status     : Stable
+=cut
 
 sub delete {
   my $self = shift;
