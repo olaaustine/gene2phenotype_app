dab5dd65fbcb47f03093fb6fe3c5c098f3c7a235
olaaustine <olaaustine@ebi.ac.uk>
2023-10-03 09:15:08 +0100

    Fixing regex stuff for publication

--- lib/Gene2phenotype/Controller/GenomicFeatureDiseasePanel.pm
+++ lib/Gene2phenotype/Controller/GenomicFeatureDiseasePanel.pm
@@ -505,7 +505,10 @@ sub add_publication {
   my $email = shift;
   my $publication = shift; 
 
-  $publication = undef if ($publication !~ /^\d+$/);
+  $publication = undef if ($publication !~ /\d$/);
+  use Data::Dumper;
+  print Dumper($publication);
+
 
   my $gfd_publication_model = $self->model('genomic_feature_disease_publication');
   
